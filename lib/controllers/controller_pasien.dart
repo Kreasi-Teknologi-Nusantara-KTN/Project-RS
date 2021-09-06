60037f387c1f523a7c3771c2e7ee359ff500d48d
CagionSource <78655390+CagionSource@users.noreply.github.com>
2021-09-06 17:47:48 +0700

    add registrasi non foto profile

--- lib/controllers/controller_pasien.dart
+++ lib/controllers/controller_pasien.dart
@@ -153,6 +153,130 @@ class ControllerPasien extends GetxController {
                 middleText: "Cek email anda untuk verifikasi",
                 onConfirm: () {
                   Get.back();
+                });
+            userCredential.user.sendEmailVerification();
+          }
+        } on FirebaseAuthException catch (e) {
+          print("error fr : " + e.toString());
+          if (e.code == 'weak-password') {
+            print('The password provided is too weak.');
+            Get.defaultDialog(title: "Error", middleText: e.code);
+          } else if (e.code == 'email-already-in-use') {
+            print('The account already exists for that email.');
+            Get.defaultDialog(title: "Error", middleText: e.code);
+          } else {
+            Get.defaultDialog(title: "Error", middleText: e.code);
+          }
+        } catch (e) {
+          print(e);
+        }
+      }
+      return value;
+    }).catchError((e) {
+      Get.defaultDialog(title: "Info", content: Text(e.toString()));
+    });
+  }
+
+  Future registerPasienController_nonGambar(
+      String nama_lengkap,
+      no_hp,
+      jenis_kelamin,
+      tanggal_lahir,
+      no_ktp,
+      agama,
+      pendidikan,
+      alamat,
+      email,
+      created_at,
+      updated_at,
+      password) async {
+    return registrasiPasienServices
+        .connectToAPI_nonGambar(
+            nama_lengkap,
+            no_hp,
+            jenis_kelamin,
+            tanggal_lahir,
+            no_ktp,
+            agama,
+            pendidikan,
+            alamat,
+            email,
+            created_at,
+            updated_at,
+            password)
+        .then((value) async {
+      print("value c : " + value.toString());
+      if (value == null) {
+        print("daftar gagal");
+      } else {
+        print("daftar lanjut verif $email  : $password");
+        try {
+          print("daftar firebase");
+          UserCredential userCredential = await FirebaseAuth.instance
+              .createUserWithEmailAndPassword(email: email, password: password);
+          print("user : " + userCredential.user.toString());
+          if (!userCredential.user.emailVerified) {
+            print("Cek email anda untuk verifikasi");
+            print("boleh lanjut");
+            String usernameAuth = "admin";
+            String passwordAuth = "1234";
+            String basicAuth = 'Basic ' +
+                base64Encode(utf8.encode('$usernameAuth:$passwordAuth'));
+            print(basicAuth);
+            String apiURL =
+                "https://api.rsbmgeriatri.com/api/Pasien?bhayangkara-key=bhayangkara123";
+
+            // Map<String, dynamic> data = {
+            //   "nama_lengkap": nama_lengkap,
+            //   "no_hp": no_hp,
+            //   "tanggal_lahir": tanggal_lahir,
+            //   "jenis_kelamin": jenis_kelamin,
+            //   "no_ktp": no_ktp,
+            //   "agama": agama,
+            //   "pendidikan": pendidikan,
+            //   "alamat": alamat,
+            //   "email": email,
+            //   "created_at": created_at,
+            //   "updated_at": updated_at,
+            //   "password": password
+            // };
+            // final regisResponse = await http.post(Uri.parse(apiURL),
+            //     headers: <String, String>{'authorization': basicAuth},
+            //     body: data);
+            // print("regisResposne " + regisResponse.statusCode.toString());
+            // if (regisResponse.statusCode == 200) {
+            //   return jsonDecode(regisResponse.body);
+            // }
+            final request = http.MultipartRequest('POST', Uri.parse(apiURL));
+            request.headers["authorization"] = basicAuth;
+            request.fields['nama_lengkap'] = nama_lengkap;
+            // request.files
+            //     .add(await http.MultipartFile.fromPath('file', gambar.path));
+            request.fields['no_hp'] = no_hp;
+            request.fields['tanggal_lahir'] = tanggal_lahir;
+            request.fields['jenis_kelamin'] = jenis_kelamin;
+            request.fields['no_ktp'] = no_ktp;
+            request.fields['agama'] = agama;
+            request.fields['pendidikan'] = pendidikan;
+            request.fields['alamat'] = alamat;
+            request.fields['email'] = email;
+            request.fields['created_at'] = created_at;
+            request.fields['updated_at'] = updated_at;
+            request.fields['password'] = password;
+
+            final streamedResponse = await request.send();
+            final response = await http.Response.fromStream(streamedResponse);
+
+            print('Status code: ${response.statusCode}');
+            print('Body: ${response.body}');
+            if (response.statusCode == 200) {
+              return jsonDecode(response.body).toString();
+            }
+            Get.defaultDialog(
+                title: "Info",
+                middleText: "Cek email anda untuk verifikasi",
+                onConfirm: () {
+                  Get.back();
                   Get.back();
                 });
             userCredential.user.sendEmailVerification();
